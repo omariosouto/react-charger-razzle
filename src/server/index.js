31405b99f5ee97dcbe961463e3612be85c1e4e0b
Mario Souto <mariosoutoskn@gmail.com>
2018-04-19 13:19:05 -0300

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ import 'universal-fetch';
 // Helpers para o SSR
 import { doRedirect,
         extractInitialData,
-        getActiveRouteFromRoutes,
+        extractActiveRouteInfoFromRoutes,
         getCurrentComponent } from './ssr-helpers.js'
 
 // Coisas do React
@@ -43,22 +43,23 @@ server
     let status = 200
 
     // 1 - Extrai a rota ativa
-    const activeRoute = getActiveRouteFromRoutes(request.url, Routes)
+    const activeRouteInfo = extractActiveRouteInfoFromRoutes(request.url, Routes)
+    request.params = activeRouteInfo.params // Repassa os parâmetros do React Router para o `request` do Express
 
     // 2 - Precisamos garantir que a rota 404 existe na aplicação.
-    if(!activeRoute) { 
+    if(!activeRouteInfo.route) { 
       resposta.send("Você deve criar uma rota que represente a 404 com o path='*'.")
       return false;
     }
     
     // 3 - Define um redirect caso necessário
-    if(activeRoute.props.render) { 
-      doRedirect(activeRoute, resposta.redirect)
+    if(activeRouteInfo.route.props.render) { 
+      doRedirect(activeRouteInfo, resposta.redirect)
       return false;
     }
 
-    // 4 - Extrai o componente
-    getCurrentComponent(activeRoute.props)
+    // 4 - Extrai o componente da rota atual (Sincrono ou Assincrono)
+    getCurrentComponent(activeRouteInfo.route.props)
       .then((component) => extractInitialData(component, request))
       .then(({component, initialData}) => {        
         // Setup da aplicação no server (obrigatório)
@@ -70,11 +71,19 @@ server
         // ./
 
         // Configuração do Redux (opcional)
-        const store = configuraStore(initialData)
+        const storeInicial = configuraStore() // Pega o esqueleto da store do redux
+        const store = configuraStore({
+          ...storeInicial.getState(),
+          ...initialData  
+        }) // Cria a store, somente alterando o dado passado, mas mantendo o esqueleto
+        
+        // Essa alteração deve ser feita, somente por conta do redux estar sendo aplicado no projeto
+        initialData = store.getState()
+
         markup = (
           <Provider store={store}>
             {markup}
-            </Provider>
+          </Provider>
         )
         // ./
 
